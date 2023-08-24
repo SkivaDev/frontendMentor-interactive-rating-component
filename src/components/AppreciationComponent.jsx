d6e7bf9c3aaea55f2ead55376e616ae0b67f2d12
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-08-23 21:24:24 -0500

    styles part1

---
+++ src/components/AppreciationComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AppreciationComponent() {
+  return (
+    <div>AppreciationComponent</div>
+  )
+}
+
+export default AppreciationComponent
---
+++ src/components/RatingComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RatingComponent() {
+  return (
+    <div>RatingComponent</div>
+  )
+}
+
+export default RatingComponent
